9621b183b4b26c573ba024c6facebef2b1f45900
Kenrick Turner <kenrickturner@gmail.com>
2012-07-08 17:19:03 +0300

    Now includes an additional anonymised dump encrypted with an independent key
    Hero now does this:
     1. Dumps the openmrs db
     2. Reloads the dump in a separate db called anonymiser
     3. Runs some SQL to remove personally identifiable information
     4. Re-dumps the anonymised db
     5. Compresses and encrypts the *non* anonymised dump with the UB Ops
    key (EEABA323)
     6. Tarballs and compresses the temp directory and /var/log
     7. Encrypts the parent archive with the UB Anonymous Data key
    (7496054D).
    
    Both encrypted archives are signed by Kasha (7FCE6178).
    
    Example directory structure of the archive:
    
    2012-07-08T135628UTC.tar.bz2.gpg
    ├── TIMESTAMP
    ├── anonymous.sql
    └── openmrs.sql.bz2.gpg
    var
    └── log

--- backup-server.sh
+++ backup-server.sh
@@ -18,8 +18,9 @@ PATH=/bin:/usr/bin
 GPGHOME=/backup/config/gnupg
 
 # What are the keyIDs that we want to encrypt and sign to?
-ENCRYPT_KEYID=0xEEABA323
-SIGN_KEYID=0x7FCE6178
+ENCRYPT_KEYID=0xEEABA323 # UB Ops team i.e. for full backup for disaster recovery
+SIGN_KEYID=0x7FCE6178 # Kasha server signing only key
+ANON_ENCRYPT_KEYID=0x7496054D # UB Anonymous Data i.e. for the logs and anonymised dump
 
 # MySQL user with SELECT privileges only on the openmrs database
 MYSQLUSER=backup
@@ -39,22 +40,101 @@ mkdir -p ${TMP}
 mysqldump -u ${MYSQLUSER} --password=${MYSQLPASS} \
 	  --compact \
 	  --single-transaction \
-          --skip-extended-insert \
-          --order-by-primary \
-          --default-character-set=latin1 \
-          openmrs > ${TMP}/openmrs.sql
+    --add-drop-table \
+    --skip-extended-insert \
+    --order-by-primary \
+    --default-character-set=latin1 \
+    openmrs > ${TMP}/openmrs.sql
+
+# Make an anonymous copy of the dump
+# Make a new temporary database and restore the dump
+mysql -u ${MYSQLUSER} --password=${MYSQLPASS} anonymiser \
+    -e "SET foreign_key_checks = 0; SOURCE ${TMP}/openmrs.sql; SET foreign_key_checks = 1;"
+
+# Anonymise data in the temporary database
+# Anonymise the following fields:
+    # 1. Delete UPN: DELETE FROM patient_identifier;
+    # 2. Change Forename: person_name.given_name
+    # 4. Change Surname: person_name.family_name
+    # 5. Delete:
+          # person_name.middle_name
+          # person_name.prefix
+          # person_name.family_name_prefix
+          # person_name.family_name2
+          # person_name.family_name_suffix
+          # person_name.degree
+    # 6. Round DoB to nearest month
+    # 7. Delete 1st line of address
+    # 8. Delete telephone number
+    # 9. Delete:
+          # Treatment supporter name
+          # Treatment supporter phone
+          # Treatment supporter address
+mysql -u ${MYSQLUSER} --password=${MYSQLPASS} anonymiser \
+    -e "SET foreign_key_checks = 0;
+        DELETE FROM patient_identifier;
+        UPDATE person_name SET given_name = 'Unknown',
+                              family_name = 'Unknown';
+        UPDATE person_name SET prefix = NULL,
+                              middle_name = NULL,
+                              family_name_prefix = NULL,
+                              family_name2 = NULL,
+                              family_name_suffix = NULL,
+                              degree = NULL;
+        UPDATE person SET birthdate = CONCAT(YEAR(birthdate),'-',LPAD(MONTH(birthdate),2,'00'),'-','01');
+        UPDATE person_address SET address1 = NULL,
+                                  latitude = NULL,
+                                  longitude = NULL;
+        DELETE FROM person_attribute WHERE person_attribute_type_id = 8;
+        DELETE FROM obs WHERE concept_id = 6252;
+        DELETE FROM obs WHERE concept_id = 6254;
+        DELETE FROM obs WHERE concept_id = 6255;
+        SET foreign_key_checks = 1;"
+
+# Dump the anonymised database
+mysqldump -u ${MYSQLUSER} --password=${MYSQLPASS} \
+    --compact \
+    --single-transaction \
+    --add-drop-table \
+    --skip-extended-insert \
+    --order-by-primary \
+    --default-character-set=latin1 \
+    anonymiser > ${TMP}/anonymous.sql
+
 
 # Create a file containing the timestamp
 echo ${TIMESTAMP} > ${TMP}/TIMESTAMP
 
-# Move everything into a timestamped directory, ready for processing
+# Move backups into a timestamped directory, ready for processing
 mkdir ${TMP}/${TIMESTAMP}
 mv ${TMP}/openmrs.sql ${TMP}/${TIMESTAMP}
+mv ${TMP}/anonymous.sql ${TMP}/${TIMESTAMP}
 mv ${TMP}/TIMESTAMP ${TMP}/${TIMESTAMP}
 
-# Compress
+# Compress the full backup
+bzip2 -z -9 ${TMP}/${TIMESTAMP}/openmrs.sql
+
+# Encrypt and sign the full backup
+gpg --homedir ${GPGHOME} \
+    --no-verbose \
+    --quiet \
+    --batch \
+    --no-tty \
+    --output ${TMP}/${TIMESTAMP}/openmrs.sql.bz2.gpg \
+    --encrypt \
+    --recipient ${ENCRYPT_KEYID} \
+    --sign \
+    --local-user ${SIGN_KEYID} \
+    --always-trust \
+    ${TMP}/${TIMESTAMP}/openmrs.sql.bz2
+
+# Remove the unencrypted full backup dump from the timestamp dir
+rm ${TMP}/${TIMESTAMP}/openmrs.sql.bz2
+
+
 cd ${TMP}
 /usr/bin/sudo /bin/tar cjf ${TIMESTAMP}.tar.bz2 ${TIMESTAMP} /var/log
+
 
 # Encrypt and sign
 gpg --homedir ${GPGHOME} \
@@ -64,7 +144,7 @@ gpg --homedir ${GPGHOME} \
     --no-tty \
     --output ${TMP}/${TIMESTAMP}.tar.bz2.gpg \
     --encrypt \
-    --recipient ${ENCRYPT_KEYID} \
+    --recipient ${ANON_ENCRYPT_KEYID} \
     --sign \
     --local-user ${SIGN_KEYID} \
     --always-trust \
